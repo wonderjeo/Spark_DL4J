55c1e6fa2fe1b4eeb2bd7bfa94afa742d593596f
王德昭 <455708864@qq.com>
2019-04-05 15:09:26 +0800

    Initial commit

---
+++ src/main/java/SimpleApp.java
@@ -0,0 +1,15 @@
+import org.apache.hadoop.conf.Configuration;
+import java.io.IOException;
+import org.apache.hadoop.fs.FileSystem;
+
+public class SimpleApp {
+    public static void main(String[] args){
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", "hdfs:///data/lr.txt");
+        try {
+            FileSystem fs = FileSystem.get(conf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

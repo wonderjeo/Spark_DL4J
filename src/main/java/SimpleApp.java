bd0abd9e1214e282c486a88321ca59e845043fd9
王德昭 <455708864@qq.com>
2019-04-06 20:27:49 +0800

    测试

--- src/main/java/SimpleApp.java
+++ src/main/java/SimpleApp.java
@@ -1,15 +1,22 @@
-import org.apache.hadoop.conf.Configuration;
-import java.io.IOException;
-import org.apache.hadoop.fs.FileSystem;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class SimpleApp {
     public static void main(String[] args){
-        Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", "hdfs://gpu18:9000/data/lr.txt");
-        try {
-            FileSystem fs = FileSystem.get(conf);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        JavaSparkContext ctx = new JavaSparkContext("yarn", "JavaWordCount",
+                System.getenv("SPARK_HOME"), JavaSparkContext.jarOfClass(SimpleApp.class));
+
+        System.out.println("spark home:" + ctx.getSparkHome());
     }
 }

a89c674a343ac987d6596742632f6d6871c9e192
王德昭 <455708864@qq.com>
2019-04-06 22:04:31 +0800

    测试读取JRDD

--- src/main/java/SimpleApp.java
+++ src/main/java/SimpleApp.java
@@ -1,3 +1,5 @@
+import org.apache.jute.RecordReader;
+import org.apache.spark.WritableConverter;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -5,6 +7,12 @@ import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.sql.Dataset;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.writable.Writable;
+import org.datavec.spark.transform.misc.StringToWritablesFunction;
+import org.deeplearning4j.spark.datavec.DataVecDataSetFunction;
+import org.nd4j.linalg.dataset.DataSet;
 import scala.Tuple2;
 
 
@@ -14,9 +22,17 @@ import java.util.regex.Pattern;
 
 public class SimpleApp {
     public static void main(String[] args){
-        JavaSparkContext ctx = new JavaSparkContext("yarn", "JavaWordCount",
-                System.getenv("SPARK_HOME"), JavaSparkContext.jarOfClass(SimpleApp.class));
+        String filePath = "hdfs:///data/fashion-mnist_test.csv";
+        JavaSparkContext sc = new JavaSparkContext();
+        JavaRDD<String> rddString = sc.textFile(filePath);
+        CSVRecordReader recordReader = new CSVRecordReader(',');
+        JavaRDD<List<Writable>> rddWriteables = rddString.map(new StringToWritablesFunction(recordReader));
 
-        System.out.println("spark home:" + ctx.getSparkHome());
+        int labelIndex = 1;
+        int numLabelClasses = 10;
+        JavaRDD<DataSet> rddDataSetClassification = rddWriteables.map(new DataVecDataSetFunction(labelIndex, numLabelClasses, false));
+        
+
+
     }
 }

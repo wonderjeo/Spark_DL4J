b1f23cbb24217a83aeb3b482343a01803a03e4c8
王德昭 <455708864@qq.com>
2019-04-06 15:49:32 +0800

    更新文件地址

--- src/main/java/SimpleApp.java
+++ src/main/java/SimpleApp.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.fs.FileSystem;
 public class SimpleApp {
     public static void main(String[] args){
         Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", "hdfs://gpu18:9000");
+        conf.set("fs.defaultFS", "hdfs://gpu18:9000/data/lr.txt");
         try {
             FileSystem fs = FileSystem.get(conf);
         } catch (IOException e) {
